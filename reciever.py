9510e9e7ed0e37518c60a4843e6713591fabdb99
osamamagdy <osamamagdy174@gmail.com>
2022-05-22 00:02:34 +0200

    socket is finished

--- reciever.py
+++ reciever.py
@@ -1,6 +1,8 @@
 ##   first, import the socket library to be used in communication   ##
 import socket
 
+from rsa import *
+
 ## After importing, you need to create a socket object  ##
 
 reciever = socket.socket()
@@ -9,7 +11,7 @@ print("Reciever socket is created successfully and ready to recieve messages")
 
 
 # Choose the port to start communicating on ( the ip in this case will be your local host)
-port = 3000
+port = 12345
 
 # Bind the port to the reciever socket 
 reciever.bind(('',port))
@@ -23,14 +25,10 @@ reciever.listen(1)
 print("Listening on the specified port")
 
 # generate you public and private keys to be used for communication
-n = 15
-e = 13
-d = 11
+pu , pr = RSA(10)
 
-
-
-cipher = ''
-mes =''
+e,n = pu
+print("Public Key is e=" + str(e) + " n="+ str(n))
 
 # wait for the sender to request connections
 sender, address = reciever.accept()
@@ -43,12 +41,25 @@ sender.send(str(e).encode())
 sender.send(str(n).encode())
 
 
-while mes != '0':
+# As long as you didn't recieve '0' as the message, keep reading
+mes =''
+while mes != 'exit()':
     
-    # waiting for messages
-    cipher = sender.recv(1024).decode()
-
-    mes = cipher
+    # waiting for message
+    # 1- decode the recieved string 
+    # 2- convert it to integer so it can be decrypted
+    cipher = int(sender.recv(1024).decode())
+    # 3- decrypt it using your private key
+    length = Decrypt(cipher,pr)
+    # 4- The first recieved character is the message length
+    # 5- loop over it and start recieving characters one by one
+    print(length)
+    mes = ''
+    for i in range(0, length):
+        print(i)
+        cipher = int(sender.recv(1024).decode())
+        num = Decrypt(cipher,pr)
+        mes += ConvertToStr(num)
 
     print('Recieved message: ', mes)
 
--- sender.py
+++ sender.py
@@ -1,5 +1,6 @@
 ##   first, import the socket library to be used in communication   ##
 import socket
+from rsa import *
 
 ## After importing, you need to create a socket object  ##
 
@@ -9,7 +10,7 @@ print("Sender socket is created successfully and ready to send messages")
 
 
 # Choose the port to start communicating on ( the ip in this case will be your local host)
-port = 3000
+port = 12345
 
 # Connect to the port where the reciever is listening 
 sender.connect(('127.0.0.1', port))
@@ -22,19 +23,30 @@ print("choosing port %s to start connections" %(port))
 e = int(sender.recv(1024).decode());
 n = int(sender.recv(1024).decode());
 
+pu = e,n
 
+print("Recieved public key is e=" + str(e) + " n=" + str(n))
 
-cipher = ''
 mes =''
 
 
-while mes != '0':
+while mes != 'exit()':
     
     # waiting for messages to be typed
     mes = input('Write your message: ')
-    cipher = mes
 
-    sender.send(cipher.encode())
+    # 1- To be general with different key sizes, we encrypt the message character by character\
+    length = len(mes)    
+    # 2- Encrypt using the public key
+    num = Encrypt(length,pu)
+    # 3- first send the length to the reciever (also encrypted)
+    sender.send(str(num).encode())
+    print(length)
+    for i in range(0,length):
+        print(i)  
+        num = ConvertToInt(mes[i])
+        cipher = Encrypt(num,pu)
+        sender.send(str(cipher).encode())
 
 print('Closing Connection.......')
 sender.close()
